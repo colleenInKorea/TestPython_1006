81e26818c6a1077295acb68cd8fc5e6f57bcaef8
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2021-10-14 22:11:51 +0900

    colleen _1014 python excercise

---
+++ Ex04.py
@@ -0,0 +1,3 @@
+#input()함수로 사용자 입력 받기 
+string =  input("인사말을 입력하세요. > ")
+print(string)
---
+++ Test02.py
@@ -0,0 +1,12 @@
+#입력 받기 
+string = input("입력 >")
+
+#출력합니다. 
+print("자료", string)
+print("자료형", type(string))
+
+#입력받기 
+string = input("입력 > ")
+
+#출력합니다. 
+# Type Error 가 뜸 , print("입력 + 100 : ", string + 100)
